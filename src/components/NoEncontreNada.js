e61a7356a028ec37093a0813c64f653b2dd082bf
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-04-17 22:15:13 -0300

    primer commit

---
+++ src/components/NoEncontreNada.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoEncontreNada = () => {
+    return (
+        <div>
+            <img src="/error-404.jpg" alt="Página no encontrada" />
+        </div>
+    );
+}
+
+export default NoEncontreNada;
